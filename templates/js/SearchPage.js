446d1076ce510a66563c40b484af2bdffc31f281
ericaddison <ericaddison@gmail.com>
2017-10-09 18:39:48 -0500

    More search update -- multi-word tags better

--- templates/js/SearchPage.js
+++ templates/js/SearchPage.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
 
     $( function() {
+        var cache = {};
         $( "#search_tags" ).autocomplete({
-          source: "services/autocomplete"
-        });
-    } );
+        source: function( request, response ) {
+            var term = request.term;
+            if ( term in cache ) {
+              response( cache[ term ] );
+              return;
+            }
+
+            $.getJSON( "services/autocomplete", request, function( data, status, xhr ) {
+              cache[ term ] = data;
+              response( data );
+            });
+            }
+        })
+    });
 
 
     $('#search_tags').keyup(function(event) {
