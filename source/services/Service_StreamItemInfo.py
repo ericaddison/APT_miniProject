88c7b5e5e5bc1174a776ceb5ff695d79ece4cf68
Patrick Sigourney <psigourney@gmail.com>
2017-10-23 07:44:06 -0500

    Added param support to StreamItemInfo service

--- source/services/Service_StreamItemInfo.py
+++ source/services/Service_StreamItemInfo.py
@@ -6,16 +6,28 @@ from source.models.NdbClasses import Stream
 
 
 #('/services/streamiteminfo', StreamItemInfoService)
+# or
+#('/services/streamiteminfo?streamid=5629499534213120')
 
 #Returns all stream items for all streams
 class StreamItemInfoService(BaseHandler):
     def get(self):
 
+        #If parameter of id=, then return only the items with that streams ID.
+        #Otherwise return all items.
+        
         self.set_content_text_json()
         response = []
+        
+        streamid = self.get_request_param("streamid")
+        
+        if (streamid == None) or (streamid == ''):        
+            my_stream_items = StreamItem.get_all_stream_items()
 
-        all_stream_items = StreamItem.get_all_stream_items()
-        for item in all_stream_items:
+        else:
+            my_stream_items = StreamItem.get_stream_items_by_key(long(streamid))
+        
+        for item in my_stream_items:
             dictItem = {"streamid": item.stream.id(),
                         "streamname": Stream.get_by_id(item.stream.id()).name,
                         "imageurl": item.URL,
@@ -24,7 +36,7 @@ class StreamItemInfoService(BaseHandler):
                         "lng": item.longitude}
                 
             response.append(dictItem)
-
+                
         print(response)
 
         self.write_response(json.dumps(response))
